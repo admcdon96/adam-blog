03d653a2d644d319e7507d740bb4b6a784a84ffa
Adam McDonald <adam.c.mcdonald23@gmail.com>
2022-08-31 16:41:37 -0400

    Add psycopg2-binary and change to PostgreSQL

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ from sqlalchemy.orm import relationship
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+import os
 
 # ----------------------- FLASK CONFIG ------------------- #
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 Base = declarative_base()
@@ -56,7 +57,7 @@ def admin_only(f):
 
 # ----------------------- DB CONFIG ------------------- #
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
