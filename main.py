d20932b3309a5bcbf938bd5db61278cdc112b70c
Adam McDonald <adam.c.mcdonald23@gmail.com>
2022-08-31 16:43:03 -0400

    Change DB vars

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def admin_only(f):
 
 # ----------------------- DB CONFIG ------------------- #
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
